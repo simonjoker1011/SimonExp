7e9b26b7808c58a203b7dcec6c303e4c7b05ccd7
simon.huang <simon.huang@nogle.com>
2017-07-25 18:40:09 +0800

    strategy resource part1

--- src/main/java/prj/jersey/simonExp/currencyrate/StrategyResource.java
+++ src/main/java/prj/jersey/simonExp/currencyrate/StrategyResource.java
@@ -1,6 +1,6 @@
 package prj.jersey.simonExp.currencyrate;
 
-import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import javax.ws.rs.GET;
@@ -9,54 +9,19 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-import org.hibernate.Session;
-
-import prj.jersey.simonExp.datas.CurrencyData;
-import util.HibernateUtil;
-import util.TimeUtil;
+import util.StrategyUtil;
 
 @Path("StrategyResource")
 public class StrategyResource {
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public Response testGet() {
+    public Response getStatics(List<Float> prices) {
+        HashMap rtnMap = new HashMap<>();
 
-        String CurrencyName = "USD";
-        String Rate = "Buying";
-        String CashSpot = "Spot";
-        String StartDate = "2017-06-01";
-        String EndDate = "2017-07-01";
+        rtnMap.put("ArithMean", StrategyUtil.arithmeticMean(prices));
+        rtnMap.put("StandardDeviation", StrategyUtil.standardDeviation(prices));
 
-        List<CurrencyData> currList = new ArrayList<>();
-        Session session = null;
-        String queryString =
-            "FROM " + CurrencyData.EntityName +
-                " c WHERE c.currName = :CurrencyName"
-                + " AND c.rate = :rate"
-                + " AND c.cashspot = :cashspot"
-                + " AND c.rateDate >= :StartDate"
-                + " AND :EndDate >= c.rateDate";
-
-        try {
-            session = HibernateUtil.getHibernateSession();
-
-            currList = session.createQuery(queryString)
-                .setParameter("CurrencyName", CurrencyName)
-                .setParameter("rate", Rate)
-                .setParameter("cashspot", CashSpot)
-                .setParameter("StartDate", TimeUtil.strToTimestamp(StartDate))
-                .setParameter("EndDate", TimeUtil.strToTimestamp(EndDate))
-                .list();
-        } catch (Exception e) {
-            e.printStackTrace();
-            return null;
-        } finally {
-            if (session != null) {
-                session.close();
-            }
-        }
-
-        return Response.ok().entity(currList).build();
+        return Response.ok().entity(rtnMap).build();
     }
 }
