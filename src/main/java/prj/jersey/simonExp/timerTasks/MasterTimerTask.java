2c0ad164eecc8daebf7f2d7812d45da60d4a8c99
simon.huang <simon.huang@nogle.com>
2017-08-08 12:07:35 +0800

    modify master task

--- src/main/java/prj/jersey/simonExp/timerTasks/MasterTimerTask.java
+++ src/main/java/prj/jersey/simonExp/timerTasks/MasterTimerTask.java
@@ -1,15 +1,19 @@
 package prj.jersey.simonExp.timerTasks;
 
+import java.util.Date;
 import java.util.List;
 
 import org.quartz.Job;
 import org.quartz.JobDetail;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
+import org.quartz.JobKey;
 import org.quartz.Scheduler;
 import org.quartz.SchedulerException;
 import org.quartz.SimpleScheduleBuilder;
 import org.quartz.Trigger;
+import org.quartz.TriggerKey;
+import org.quartz.impl.matchers.GroupMatcher;
 
 import util.SchedulerUtil;
 
@@ -38,33 +42,36 @@ public class MasterTimerTask implements Job, Runnable {
 
     @Override
     public void run() {
+        System.out.println();
+        System.out.println("Master task fired at: " + new Date().toString());
         List<Scheduler> schedulers = SchedulerUtil.schedulers;
-        for (Scheduler scheduler : schedulers) {
+        for (Scheduler sched : schedulers) {
             try {
-                System.out.println("---------------------------");
+                System.out.println("----------------- Jobs -------------------");
+                for (String group : sched.getJobGroupNames()) {
+                    // enumerate each job in group
+                    for (JobKey jobKey : sched.getJobKeys(GroupMatcher.groupEquals(group))) {
+                        System.out.println("Found job identified by: " + jobKey);
 
-                System.out.println("-> Execution Jobs: ");
-                scheduler.getCurrentlyExecutingJobs().forEach(j -> {
-                    System.out.println(j.getFireInstanceId());
-                    System.out.println(j.getFireTime());
-                    System.out.println(j.getNextFireTime());
-                    System.out.println(j.getJobDetail().getKey().toString());
-                    System.out.println(j.getTrigger().getKey().toString());
-                });
-                System.out.println("-> Scheduler Id: ");
-                System.out.println(scheduler.getSchedulerInstanceId());
-                System.out.println("-> Scheduler Name: ");
-                System.out.println(scheduler.getSchedulerName());
-                System.out.println("-> Scheduler Hash: ");
-                System.out.println(scheduler.hashCode());
-
-                System.out.println("---------------------------");
-
+                        for (Trigger trigger : sched.getTriggersOfJob(jobKey)) {
+                            System.out.println("    \\-------> With triggers: " + trigger.getKey());
+                            System.out.println("        \\-------> With next fired at: " + trigger.getNextFireTime().toString());
+                        }
+                    }
+                }
+                System.out.println("----------------- Triggers -------------------");
+                for (String group : sched.getTriggerGroupNames()) {
+                    // enumerate each trigger in group
+                    for (TriggerKey triggerKey : sched.getTriggerKeys(GroupMatcher.groupEquals(group))) {
+                        System.out.println("Found trigger identified by: " + triggerKey);
+                    }
+                }
             } catch (SchedulerException e) {
                 // TODO Auto-generated catch block
                 e.printStackTrace();
             }
         }
+        System.out.println();
     }
 
 }
